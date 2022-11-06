fbdaca0b425972ff7610885aa7dbf9b8053e6f6a
Julram3232 <48529138+NerdRez@users.noreply.github.com>
2022-11-06 01:45:37 -0700

    Commented out UI code for easier access

--- CSC 335 Assignment 3/src/XTank.java
+++ CSC 335 Assignment 3/src/XTank.java
@@ -11,19 +11,19 @@ public class XTank {
 		try (var socket = new Socket("127.0.0.1", 59896)) {
 			DataInputStream in = new DataInputStream(socket.getInputStream());
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-			int start = 0;
-			if (in.readInt() == 0) {
-				var host = new XTankHostDisplay();
-				while (start == 0) {
-					start = host.start(); } 
-			}
-			
-			start = 0;
-			while (start == 0) {
-				var create = new PlayerCreateDisplay();
-				start = create.start(); }
-			
-			out.writeInt(1);
+			//int start = 0;
+//			if (in.readInt() == 0) {
+//				var host = new XTankHostDisplay();
+//				while (start == 0) {
+//					start = host.start(); } 
+//			}
+//			
+//			start = 0;
+//			while (start == 0) {
+//				var create = new PlayerCreateDisplay();
+//				start = create.start(); }
+//			
+//			out.writeInt(1);
 			var ui = new XTankUI(in, out);
 			ui.start(); }
 	}
