58aa64d6a405988b2aefd8e1ddbe16ed7e704730
Julram3232 <48529138+NerdRez@users.noreply.github.com>
2022-11-09 03:46:23 -0700

    Incorporated Bullet subclasses into Bullet class, deleted DefaultBullet

--- CSC 335 Assignment 3/src/Bullet.java
+++ CSC 335 Assignment 3/src/Bullet.java
@@ -1,3 +1,4 @@
+
 /*
 * AUTHOR: Julius Ramirez [modified David code]
 * FILE: Bullet.java
@@ -7,12 +8,83 @@
 * Subclasses of bullet include: Defaulty (DefaultBullet), Quicky (QuickBullet), and Biggy (BigBullet).
 * 
 */
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Rectangle;
 
 public abstract class Bullet {
+	protected int[] coordinates; // [x,y]
+	protected int shiftX;
+	protected int shiftY;
+	protected Color color;
+
+	public Bullet(Color color, int x, int y, int shiftX, int shiftY) {
+		this.color = color;
+		this.coordinates = new int[] { x, y };
+		this.shiftX = shiftX;
+		this.shiftY = shiftY;
+	}
+
 	public abstract void draw(GC gc);
-	public abstract boolean withinBounds(Rectangle bounds);
-	public abstract int[] getCoordinates();
-	public abstract boolean hasHit(Rectangle rect);
+
+	public boolean withinBounds(Rectangle bounds) {
+		if (coordinates[0] < 0 || coordinates[1] < 0 || coordinates[0] > bounds.width
+				|| coordinates[1] > bounds.height) {
+			return false;
+		}
+		return true;
+	}
+
+	public int[] getCoordinates() {
+		return coordinates;
+
+	}
+
+	public boolean hasHit(Rectangle rect) {
+		return rect.contains(coordinates[0], coordinates[1]);
+	}
+}
+
+class DefaultBullet extends Bullet {
+
+	public DefaultBullet(Color color, int x, int y, int shiftX, int shiftY) {
+		super(color, x, y, shiftX, shiftY);
+	}
+
+	@Override
+	public void draw(GC gc) {
+		gc.setBackground(color);
+		coordinates[0] += shiftX;
+		coordinates[1] += shiftY;
+		gc.fillRectangle(coordinates[0], coordinates[1], 5, 5);
+	}
+}
+
+class QuickBullet extends Bullet {
+
+	public QuickBullet(Color color, int x, int y, int shiftX, int shiftY) {
+		super(color, x, y, shiftX, shiftY);
+	}
+
+	@Override
+	public void draw(GC gc) {
+		gc.setBackground(color);
+		coordinates[0] += shiftX;
+		coordinates[1] += shiftY;
+		gc.fillRectangle(coordinates[0], coordinates[1], 2, 2);
+	}
+}
+
+class BigBullet extends Bullet {
+	public BigBullet(Color color, int x, int y, int shiftX, int shiftY) {
+		super(color, x, y, shiftX, shiftY);
+	}
+
+	@Override
+	public void draw(GC gc) {
+		gc.setBackground(color);
+		coordinates[0] += shiftX;
+		coordinates[1] += shiftY;
+		gc.fillRectangle(coordinates[0], coordinates[1], 7, 7);
+	}
 }
