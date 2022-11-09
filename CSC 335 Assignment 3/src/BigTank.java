0f0c3b831644aa76930298335f788560f17e406a
Julram3232 <48529138+NerdRez@users.noreply.github.com>
2022-11-09 02:16:45 -0700

    Created outline for new tank classes

---
+++ CSC 335 Assignment 3/src/BigTank.java
@@ -0,0 +1,47 @@
+import org.eclipse.swt.graphics.GC;
+
+public class BigTank extends Tank {
+
+	@Override
+	public void turnRight() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnLeft() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveForward() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveBackward() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void shoot() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(GC gc1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void drawBullets(GC gc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
