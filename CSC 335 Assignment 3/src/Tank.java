1ff4eea7b2630eff6bb5111f7ca78d0ad6a7e51c
kille <kille@GeoBook_240>
2022-11-10 21:52:13 -0700

    Tank has base, subclass only has getType method

--- CSC 335 Assignment 3/src/Tank.java
+++ CSC 335 Assignment 3/src/Tank.java
@@ -30,7 +30,8 @@ public abstract class Tank implements Serializable {
 	protected int[] yState;
 	protected int health;
 	protected transient List<Bullet> bulletList;
-	
+	protected Rectangle base;
+	protected int rotateMult;
 	/*
 	 * This constructor sets the color of the tanks body, arm, and also starting position.
 	 */
@@ -40,6 +41,28 @@ public abstract class Tank implements Serializable {
 		this.armColor = color2;
 		this.bulletList = new ArrayList<>();
 	} 
+	/*
+	 * This method draws the tank onto the canvas. Because tanks can be rotated, it's necessary to 
+	 * briefly translate the tank so that it rotates around itself rather than rotating around
+	 * the origin of the canvas. 
+	 */
+	public void draw(GC gc) {
+		Transform transform = new Transform(gc.getDevice());
+		gc.setBackground(armColor);
+		gc.fillRectangle(base); 									//draw base before translate
+		transform.translate(state[0] + base.width/2, state[1]+base.height/2);
+		transform.rotate(45 * rotateState);
+		gc.setTransform(transform);
+		gc.setBackground(color);
+		gc.fillRectangle(-width / 2, -height / 2, width, height);	//draw top
+		gc.setBackground(armColor);
+		gc.fillOval(-width / 2, -height / 4, width, width); 		//draw circle
+		gc.setLineWidth(4);
+		gc.drawLine(0, 0, 0, barrel);								//draw barrel
+		transform = new Transform(gc.getDevice()); 	// for some reason this line fixes bug where shapes get
+													// drawn based on tank origin instead of canvas origin
+		gc.setTransform(transform);
+	}
 	/*
 	 * This method increases the rotate state by one then checks if the number is
 	 * bounded correctly
@@ -75,14 +98,20 @@ public abstract class Tank implements Serializable {
 	public void moveForward(List<Wall> walls) {
 		int tempX = state[0] + xState[rotateState] ;
 		int tempY = state[1] + yState[rotateState];
+		int oldX = this.base.x;
+		int oldY = this.base.y;
+		this.base.x = tempX;
+		this.base.y = tempY;
 		for(Wall wall:walls) {
-			if(wall.wall.contains(tempX, tempY)) {
+			if(wall.wall.intersects(base)) {
+				this.base.x = oldX;
+				this.base.y = oldY;
 				return;
 			}
 		}
 		state[1] = tempY;
 		state[0] = tempX;
-
+		
 	}
 	/*
 	 * This method updates the X and Y values of the tank by subtracting the proper
@@ -91,46 +120,44 @@ public abstract class Tank implements Serializable {
 	public void moveBackward(List<Wall> walls) {
 		int tempX = state[0] - xState[rotateState] ;
 		int tempY = state[1] - yState[rotateState];
+		int oldX = this.base.x;
+		int oldY = this.base.y;
+		this.base.x = tempX;
+		this.base.y = tempY;
 		for(Wall wall:walls) {
-			if(wall.wall.contains(tempX, tempY)) {
+			if(wall.wall.intersects(base)) {
+				this.base.x = oldX;
+				this.base.y = oldY;
 				return;
 			}
 		}
 		state[1] = tempY;
 		state[0] = tempX;
-
+		
 	}
-	/*
-	 * This method draws the tank onto the canvas. Because tanks can be rotated, it's necessary to 
-	 * briefly translate the tank so that it rotates around itself rather than rotating around
-	 * the origin of the canvas. 
-	 */
-	public void draw(GC gc) {
-		Transform transform = new Transform(gc.getDevice());
-		transform.translate(state[0], state[1]);
-		transform.rotate(45 * rotateState);
-		gc.setTransform(transform);
-		gc.setBackground(color);
-		gc.fillRectangle(-width / 2, -height / 2, width, height);
-		gc.setBackground(armColor);
-		gc.fillOval(-width / 2, -height / 4, width, width); 
-		gc.setLineWidth(4);
-		gc.drawLine(0, 0, 0, barrel);
-		transform = new Transform(gc.getDevice()); 	// for some reason this line fixes bug where shapes get
-													// drawn based on tank origin instead of canvas origin
-		gc.setTransform(transform);
-	}
+	
 	/*
 	 * This method is responsible for drawing a specific tanks bullets that they have shot.
 	 * It calls the bullet's draw() method and then checks to see if the bullet is still within
 	 * bounds, destroying it if it is out of the map or TODO hits a wall. 
 	 */
-	public void drawBullets(GC gc) {
+	public void drawBullets(GC gc,List<Wall> walls) {
 		for(int i = 0; i < bulletList.size();i++) {
 			bulletList.get(i).draw(gc);
 			if(!bulletList.get(i).withinBounds(gc.getDevice().getBounds())) {
+				System.out.println("Bullet has gone out of bounds! Deleting bullet");
 				bulletList.remove(i);
 				i--;
+			}
+			else {
+				for(Wall wall: walls) {
+					if(wall.wall.contains(bulletList.get(i).coordinates[0], bulletList.get(i).coordinates[1])) {
+						System.out.println("Bullet has touched a wall! Deleting bullet");
+						bulletList.remove(i);
+						i--;
+						break;
+					}
+				}
 			}
 		}
 	}
@@ -144,7 +171,12 @@ public abstract class Tank implements Serializable {
 	 * bulletList so that the bullet may be drawn and possibly interact 
 	 * with other enemy tanks in the future.
 	 */
-	public abstract void shoot();
+	public void shoot() {
+		int xOffset = this.xState[rotateState]*rotateMult;
+		int yOffset = this.yState[rotateState]*rotateMult;
+		bulletList.add(new DefaultBullet(color, this.state[0]+this.base.width/2+xOffset,this.state[1]+this.base.height/2+yOffset,
+				this.xState[rotateState],this.yState[rotateState]));
+	}
 	public boolean hasHit(Rectangle rect) { //TODO implement method
         for(Bullet bullet:bulletList) {
             if (bullet.hasHit(rect)) {
@@ -166,21 +198,13 @@ class DefaultTank extends Tank implements Serializable{
 		this.width = 25;
 		this.height = 50;
 		this.barrel = (int) -(height * (.75));
+		this.base = new Rectangle(300,500,55,55);
+		this.rotateMult = 3;
 	}
 
 	@Override
 	public String getType() {
 		return "Defaulty";
-	}
-
-	@Override
-	public void shoot() {
-		int rotateMult = 3;
-		int xOffset = this.xState[rotateState]*rotateMult;
-		int yOffset = this.yState[rotateState]*rotateMult;
-		bulletList.add(new DefaultBullet(color, this.state[0]+xOffset,this.state[1]+yOffset,
-				this.xState[rotateState],this.yState[rotateState]));
-	
 	}
 	
 }
@@ -195,23 +219,12 @@ class QuickTank extends Tank implements Serializable{
 		this.width = 25;
 		this.height = 30;
 		this.barrel = (int) -(height * (.75));
+		this.base = new Rectangle(300,500,35,35);
+		this.rotateMult = 1;
 	}
 	@Override
 	public String getType() {
 		return "Quicky";
-	}
-	/*
-	 * This method 'shoots' a bullet by adding a bullet to the tanks 
-	 * bulletList so that the bullet may be drawn and possibly interact 
-	 * with other enemy tanks in the future.
-	 */
-	@Override
-	public void shoot() {
-		int rotateMult = 1;
-		int xOffset = this.xState[rotateState]*rotateMult;
-		int yOffset = this.yState[rotateState]*rotateMult;
-		bulletList.add(new QuickBullet(color, this.state[0]+xOffset,this.state[1]+yOffset,
-				this.xState[rotateState],this.yState[rotateState]));
 	}
 }
 class BigTank extends Tank implements Serializable{
@@ -227,20 +240,13 @@ class BigTank extends Tank implements Serializable{
 		this.width = 40;
 		this.height = 55;
 		this.barrel = (int) -(height * (.75));
+		this.base = new Rectangle(800,500,60,60);
+		this.rotateMult = 6;
 	}
 
 	@Override
 	public String getType() {
 		return "Biggy";
-	}
-
-	@Override
-	public void shoot() {
-		int rotateMult = 6;
-		int xOffset = this.xState[rotateState]*rotateMult;
-		int yOffset = this.yState[rotateState]*rotateMult;
-		bulletList.add(new BigBullet(color, this.state[0]+xOffset,this.state[1]+yOffset,
-		this.xState[rotateState],this.yState[rotateState]));
 	}
 	
 }
